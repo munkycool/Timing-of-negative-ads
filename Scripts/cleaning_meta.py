4abf75890bdf504a80f342230b6c6a5ac15b13cc
Artemis Tack <artemis@art3mix.com>
2025-08-21 14:58:49 -0500

    Implement CSV file cleaning functionality in cleaning_meta.py

--- Scripts/cleaning_meta.py
+++ Scripts/cleaning_meta.py
@@ -1,1 +1,13 @@
 import pandas as pd
+
+# ask for the file path
+file_name = input("Enter the path to the CSV file: ")
+
+data = pd.read_csv(file_name)
+
+# drop duplicates in the 7th column
+cleaned_data = data.drop_duplicates(subset=data.columns[7])
+
+cleaned_data.to_csv('cleaned_meta.csv', index=False)
+
+
